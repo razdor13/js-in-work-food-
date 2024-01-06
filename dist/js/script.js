6eb623786854385c4385af881a3f825630c697aa
Razdor13 <oneamaday@gmail.com>
2024-01-06 21:23:16 +0200

    add class MenuCard for dinamic create card with content

--- dist/js/script.js
+++ dist/js/script.js
@@ -130,6 +130,40 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
   window.addEventListener("scroll", showModalByScroll);
+  // use class for card
+  class MenuCard {
+    constructor(src, alt, title, descr, price, parentSelector) {
+      this.src = src;
+      this.alt = alt;
+      this.title = title;
+      this.descr = descr;
+      this.price = price;
+      this.transfer = 27;
+      this.perent = document.querySelector(parentSelector);
+      this.changeToUah();
+    }
+    changeToUah() {
+      this.price = this.price * this.transfer;
+    }
+    render() {
+      const element = document.createElement("div");
+      element.innerHTML = `
+            <div class="menu__item">
+                    <img src=${this.src} alt=${this.alt}>
+                    <h3 class="menu__item-subtitle">${this.title}</h3>
+                    <div class="menu__item-descr">${this.descr}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                    </div>
+                </div>`;
+      this.perent.append(element);
+    }
+  }
+  new MenuCard("img/tabs/vegy.jpg", "vegy", 'Меню "Фитнес"', 'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих qweqweqweqweqweqweqweqweqweqwe', 9, ".menu .container").render();
+  new MenuCard("img/tabs/vegy.jpg", "vegy", 'Меню "Фитнес"', 'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих qweqweqweqweqweqweqweqweqweqwe', 9, ".menu .container").render();
+  new MenuCard("img/tabs/vegy.jpg", "vegy", 'Меню "Фитнес"', 'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих qweqweqweqweqweqweqweqweqweqwe', 9, ".menu .container").render();
 });
 /******/ })()
 ;
